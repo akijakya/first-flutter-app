4c6e1dd48668f4aa7c91acc4c025964f306b315f
András Jáky <jakyandras@gmail.com>
2023-10-11 00:33:46 +0200

    feat: add favorites page
    Signed-off-by: András Jáky <jakyandras@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -188,6 +188,42 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            'You have ${appState.favorites.length} favorites:',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(
+              Icons.favorite,
+              size: 16,
+            ),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
